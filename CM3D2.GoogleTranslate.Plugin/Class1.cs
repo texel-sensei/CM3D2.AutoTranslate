79ba1400d26a9231c7408b0950a400a213746bd7
Johannes Pieger <johannes.pieger@posteo.de>
2017-01-17 22:08:58 +0100

    Add project files.

---
+++ CM3D2.GoogleTranslate.Plugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM3D2.GoogleTranslate.Plugin
+{
+    public class Class1
+    {
+    }
+}
