4b98973570c0263a3a3709a0c6ff62123d2b9d9e
Texel-Sensei <texelsensei@posteo.net>
2017-11-15 18:50:32 +0100

    Fix bug with hook and UTL.
    The name of the UnifiedTranslationLoader hook was wrongfully set as "UTLHook" instead of the correct classname. Fixed the string entry and added an error check to the code.

--- CM3D2.GoogleTranslate.Plugin/HookHelper.cs
+++ CM3D2.GoogleTranslate.Plugin/HookHelper.cs
@@ -40,7 +40,7 @@ namespace CM3D2.AutoTranslate.Plugin
 			{
 				AssemblyID = ".Translation",
 				EventName = "TranslateText",
-			    HandlerModuleName = "UTLHook"
+			    HandlerModuleName = "UnifiedTranslationLoaderHook"
             } },
 			{ParentTranslationPlugin.YetAnotherTranslator, new PluginHookInfo()
 			{
@@ -97,9 +97,16 @@ namespace CM3D2.AutoTranslate.Plugin
 		    {
 		        var curAssembly = Assembly.GetAssembly(typeof(TranslationPluginHook));
 		        var namespace_ = "CM3D2.AutoTranslate.Plugin.Hooks";
-		        var hookType = curAssembly.GetType(
-		            namespace_ + "." + _hook.Info.HandlerModuleName
+		        var hookName = namespace_ + "." + _hook.Info.HandlerModuleName;
+                var hookType = curAssembly.GetType(
+		            hookName
 		        );
+
+		        if (hookType == null)
+		        {
+		            Logger.LogError($"Failed to get hook loader type '{hookName}'. This is a bug, aborting plugin!");
+		            return false;
+		        }
 
 		        var handler =  Activator.CreateInstance(hookType, new object[]{atp}) as TranslationPluginHook;
 		        if (handler == null)
