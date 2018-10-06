22059e774d5a4f455699b2b806ab3c0c773299e9
DerProbber <derprobber@gmail.com>
2018-10-06 18:35:07 +0200

    Using the official Google API to prevent 503 service unavailable error.

--- CM3D2.GoogleTranslate.Plugin/GoogleTranslationModule.cs
+++ CM3D2.GoogleTranslate.Plugin/GoogleTranslationModule.cs
@@ -8,12 +8,22 @@ using UnityEngine;
 
 namespace CM3D2.AutoTranslate.Plugin
 {
-	internal class GoogleTranslationModule : TranslationModule
+    [Serializable]
+    public class trans
+    {
+        public string q;
+        public string source;
+        public string target;
+        public string format;
+    }
+
+    internal class GoogleTranslationModule : TranslationModule
 	{
 		public override string Section => "Google";
 		private string _targetLanguage = "en";
+        private string _apiKey = "";
 
-		public override bool Init()
+        public override bool Init()
 		{
 			// Do Nothing
 			StartCoroutine(Test());
@@ -51,7 +61,8 @@ namespace CM3D2.AutoTranslate.Plugin
 		protected override void LoadConfig(CoreUtil.SectionLoader section)
 		{
 			section.LoadValue("TargetLanguage", ref _targetLanguage);
-		}
+            section.LoadValue("APIKey", ref _apiKey);
+        }
 
 		public override IEnumerator Translate(TranslationData data)
 		{
@@ -71,34 +82,10 @@ namespace CM3D2.AutoTranslate.Plugin
 
 		private static string ExtractTranslationFromGoogleString(string input)
 		{
-			var data = JSON.Parse(input);
-			var lineBuilder = new StringBuilder(input.Length);
-			foreach (JSONNode entry in data.AsArray[0].AsArray)
-			{
-				var token = entry.AsArray[0].ToString();
-
-				if (lineBuilder.Length != 0) lineBuilder.Append(" ");
-				lineBuilder.Append(token.Substring(1, token.Length - 2));
-			}
-
-			var text = lineBuilder.ToString();
-			var builder = new StringBuilder(text.Length);
-			for (var i = 0; i < text.Length; ++i)
-			{
-				if (text[i] == '\\')
-				{
-					// skip this and next token
-					if (text[i + 1] != '"')
-						i++;
-					continue;
-				}
-				if (text[i] == '"' && text[i - 1] != '\\')
-					break;
-				builder.Append(text[i]);
-			}
-
-			return builder.ToString();
-		}
+            var data = JSON.Parse(input);
+            var translatedText = data["data"]["translations"][0]["translatedText"];
+            return translatedText;
+        }
 
 		private IEnumerator TranslateGoogle(string text, string fromCulture, string toCulture, TranslationData translation)
 		{
@@ -119,13 +106,19 @@ namespace CM3D2.AutoTranslate.Plugin
 			if (tokens.Length > 1)
 				toCulture = tokens[0];
 
-			//string url =
-			//	$@"http://translate.google.com/translate_a/t?client=j&text={WWW.EscapeURL(text)}&hl=en&sl={fromCulture}&tl={toCulture}";
+            trans tr = new trans();
+            tr.q = text;
+            tr.source = fromCulture;
+            tr.target = toCulture;
+            tr.format = "text";
 
-			string url = $@"https://translate.googleapis.com/translate_a/single?client=gtx&sl={fromCulture}&tl={toCulture}&dt=t&q={WWW.EscapeURL(text)}";
+            string transjson = JsonUtility.ToJson(tr);
+            var bytes = Encoding.UTF8.GetBytes(transjson);
 
-			var headers = new Dictionary<string, string> { { "User-Agent", "Mozilla/5.0" }, { "Accept-Charset", "UTF-8" } };
-			var www = new WWW(url, null, headers);
+            string url = "https://translation.googleapis.com/language/translate/v2?key=" + _apiKey;
+
+            var headers = new Dictionary<string, string> { { "User-Agent", "Mozilla/5.0" }, { "Accept-Charset", "UTF-8" } };
+			var www = new WWW(url, bytes, headers);
 			yield return www;
 
 
